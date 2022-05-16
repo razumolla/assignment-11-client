191a09d774d7608fb5cfabe8a18917fe337b7333
razumolla <rajuahmed9344@gmail.com>
2022-05-16 12:19:02 +0600

    Banner added

--- src/components/pages/Home/Banner/Banner.js
+++ src/components/pages/Home/Banner/Banner.js
@@ -9,7 +9,38 @@ const Banner = () => {
     };
     return (
         <>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img className="d-block w-100" src={`https://i.ibb.co/42LCq4y/ban1.png`} alt="First slide" />
+                    <Carousel.Caption>
+                        <h4>New Super Car</h4>
+                    </Carousel.Caption>
+                </Carousel.Item>
 
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={`https://i.ibb.co/0qJ7Sjg/ban2.png`}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>10% Discount </h3>
+                        <h6>Minimum Order 50 cars... </h6>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={`https://i.ibb.co/tK8YYVd/ban3.png`}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3> Latest New BMW </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </>
     );
 };
